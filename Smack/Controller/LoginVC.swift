6c5838a887a4ea8823aa9c4316dbbf53fe1343f3
Alexander Kufel <kufelalexander@gmail.com>
2018-08-04 18:46:25 +0300

    Added LoginVC(logic)

--- Smack/Controller/LoginVC.swift
+++ Smack/Controller/LoginVC.swift
@@ -9,11 +9,17 @@
 import UIKit
 
 class LoginVC: UIViewController {
+    
+    // Outlets
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
+        setupView()
     }
 
     @IBAction func closePressed(_ sender: Any) {
@@ -24,4 +30,33 @@ class LoginVC: UIViewController {
         performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
     }
     
+    @IBAction func loginPressed(_ sender: Any) {
+        spinner.isHidden = false
+        spinner.startAnimating()
+        guard let email = usernameTxt.text, usernameTxt.text != "" else { return }
+        guard let password = passwordTxt.text, passwordTxt.text != "" else { return }
+        
+        AuthService.instance.loginUser(email: email, password: password) { (success) in
+            if success {
+                AuthService.instance.findUserByEmail(completion: { (success) in
+                    if success {
+                        NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+                        self.spinner.isHidden = true
+                        self.spinner.stopAnimating()
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                })
+            }
+        }
+        
+        
+        
+    }
+    
+    func setupView() {
+        spinner.isHidden = true
+        usernameTxt.attributedPlaceholder = NSAttributedString(string: "Username", attributes: [NSAttributedString.Key.foregroundColor : smackPurplePlacehoder])
+        passwordTxt.attributedPlaceholder = NSAttributedString(string: "Username", attributes: [NSAttributedString.Key.foregroundColor : smackPurplePlacehoder])
+        
+    }
 }
