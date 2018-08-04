f50d0667ad707296f97115cd4f31a780ed6c3647
Alexander Kufel <kufelalexander@gmail.com>
2018-08-04 11:02:05 +0300

    Added AvatarVC(UI Elements) / AvatarCell clacc

---
+++ Smack/View/AvatarCell.swift
@@ -0,0 +1,25 @@
+//
+//  AvatarCell.swift
+//  Smack
+//
+//  Created by user on 04.08.2018.
+//  Copyright © 2018 user. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
+    }
+    
+    func setupView() {
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10.0
+        self.clipsToBounds = true
+    }
+}
