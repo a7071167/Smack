06e3c45ee1cae64a47740228145782c63d5489da
Alexander Kufel <kufelalexander@gmail.com>
2018-08-06 08:32:03 +0300

    Added socket for create new Channel

---
+++ Smack/Servises/SocketService.swift
@@ -0,0 +1,47 @@
+//
+//  SocketService.swift
+//  Smack
+//
+//  Created by user on 06.08.2018.
+//  Copyright © 2018 user. All rights reserved.
+//
+
+import UIKit
+import SocketIO
+
+class SocketService: NSObject {
+
+    static let instance = SocketService()
+    
+    override init() {
+        super.init()
+    }
+    
+    var manager = SocketManager(socketURL: URL(string: BASE_URL)!)
+    lazy var socket = manager.defaultSocket
+    
+    func establishConnection() {
+        socket.connect()
+    }
+    
+    func closeConnection() {
+        socket.disconnect()
+    }
+    
+    func addChannel(channelName: String, channelDescription: String, completion: @escaping CompletionHandler) {
+        socket.emit("newChannel", channelName, channelDescription)
+        completion(true)
+    }
+    
+    func getChannel(completion: @escaping CompletionHandler) {
+        socket.on("channelCreated") { (dataArray, ack) in
+            guard let channelName = dataArray[0] as? String else { return }
+            guard let channelDescription = dataArray[1] as? String else { return }
+            guard let channelId = dataArray[2] as? String else { return }
+            
+            let newChannel = Channel.init(name: channelName, description: channelDescription, _id: channelId)
+            MessageService.instance.channels.append(newChannel)
+            completion(true)
+        }
+    }
+}
