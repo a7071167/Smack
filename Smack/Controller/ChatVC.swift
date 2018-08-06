a26afd76beaaf18747fb91328aeefd1c154477ae
Alexander Kufel <kufelalexander@gmail.com>
2018-08-06 13:59:06 +0300

    Adding func addMessege and getMessage

--- Smack/Controller/ChatVC.swift
+++ Smack/Controller/ChatVC.swift
@@ -13,11 +13,15 @@ class ChatVC: UIViewController {
     // Outlets
     @IBOutlet weak var menuBtn: UIButton!
     @IBOutlet weak var channelNameLbl: UILabel!
+    @IBOutlet weak var messageTxtBox: UITextField!
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.bindToKeyboard()
+        let tap = UITapGestureRecognizer(target: self, action: #selector(ChatVC.handleTap(_:)))
+        view.addGestureRecognizer(tap)
         menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
         self.view.addGestureRecognizer((self.revealViewController().panGestureRecognizer()))
         self.view.addGestureRecognizer((self.revealViewController().tapGestureRecognizer()))
@@ -44,11 +48,28 @@ class ChatVC: UIViewController {
     @objc func channelSelected(_ notif: Notification) {
         updateWithChannel()
     }
+    @objc func handleTap(_ recognizer: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
     
     func updateWithChannel() {
         let channelName = MessageService.instance.selectedChannel?.description ?? ""
         channelNameLbl.text = "#\(channelName)"
         getMessages()
+    }
+    
+    @IBAction func sendMessagePressed(_ sender: Any) {
+        if AuthService.instance.isLoggedIn {
+            guard let channelId = MessageService.instance.selectedChannel?._id else { return }
+            guard let message = messageTxtBox.text else { return }
+            
+            SocketService.instance.addMessage(messageBody: message, userId: UserDataService.instance.id, channelId: channelId) { (success) in
+                if success {
+                    self.messageTxtBox.text = ""
+                    self.messageTxtBox.resignFirstResponder()
+                }
+            }
+        }
     }
     
     func onLoginGetMessages() {
